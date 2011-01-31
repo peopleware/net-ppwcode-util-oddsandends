d3732c1f1297eb7abcfa05aff99431a8224ebbc7
Jan Dockx <jan_dockx@peopleware.be>
2011-01-31 11:46:14 +0000

    structure for 1.1
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Util/OddsAndEnds/I/1.n/1.1@4668 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ solution/I/Exceptions/ValidationException.cs
@@ -0,0 +1,54 @@
+﻿//Copyright 2004 - $Date: 2008-11-15 23:58:07 +0100 (za, 15 nov 2008) $ by PeopleWare n.v..
+
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+
+//http://www.apache.org/licenses/LICENSE-2.0
+
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PPWCode.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public ValidationException(object sender, string property, object oldValue, object newValue, string message)
+            : base(message)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+            PropertyName = property;
+            Sender = sender;
+        }
+
+        public object OldValue
+        {
+            get;
+            private set;
+        }
+        public object NewValue
+        {
+            get;
+            private set;
+        }
+        public string PropertyName
+        {
+            get;
+            private set;
+        }
+        public object Sender
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ solution/Test_I/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PPWCode.Vernacular.Exceptions.Test_I")]
+[assembly: AssemblyDescription("Test code of the PPWCode Exceptions Vernacular")]
+[assembly: AssemblyConfiguration("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c52ee8f-d53d-42d9-b7ad-a1cf552e0006")]
