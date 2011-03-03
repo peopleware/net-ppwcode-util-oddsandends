4e8bea31c0528fb10451e43e73eb50c35da6c34f
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2011-03-03 19:07:08 +0000

    * Adding helper code for handling Active Directory Search
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Util/OddsAndEnds/trunk@4834 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ Test_I/ADSearchTest.cs
@@ -0,0 +1,25 @@
+﻿#region Using
+
+using System.Security.Principal;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PPWCode.Util.OddsAndEnds.I.ActiveDirectory;
+
+#endregion
+
+namespace PPWCode.Util.OddsAndEnds.Test_I
+{
+    [TestClass]
+    public class AdSearchTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            WindowsIdentity wi = WindowsIdentity.GetCurrent();
+            Assert.IsNotNull(wi);
+            AdSearch adSearch = new AdSearch(AdSearch.GetDomainFromUserAccount(wi.Name));
+            Assert.IsTrue(adSearch.UserExists(wi.Name));
+        }
+    }
+}
