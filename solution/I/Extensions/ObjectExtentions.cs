20aa2765b91a76ce29d7e1e2cb5326be5707244b
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2011-03-10 14:44:12 +0000

    Release 1.5
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Util/OddsAndEnds/I/latest@4877 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ solution/I/Extensions/ObjectExtentions.cs
@@ -0,0 +1,60 @@
+﻿//Copyright 2004 - $Date: 2008-11-15 23:58:07 +0100 (za, 15 nov 2008) $ by PeopleWare n.v..
+
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+
+//http://www.apache.org/licenses/LICENSE-2.0
+
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+#region Using
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+#endregion
+
+namespace PPWCode.Util.OddsAndEnds.I.Extensions
+{
+    public static class ObjectExtentions
+    {
+        [Pure]
+        public static string ToLogString(this object obj)
+        {
+            if (obj != null)
+            {
+                StringBuilder sb = new StringBuilder(obj.ToString());
+                {
+                    sb.Append("{ ");
+                    sb.AppendFormat("HashCode = '{0}'", obj.GetHashCode());
+
+                    foreach (PropertyInfo prop in obj.GetType().GetProperties().Where(prop => prop.PropertyType.IsValueType))
+                    {
+                        object value;
+                        try
+                        {
+                            value = prop.GetValue(obj, null);
+                        }
+                        catch (Exception e)
+                        {
+                            value = e.GetBaseException().Message;
+                        }
+                        sb.AppendFormat(", {0} = '{1}'", prop.Name, value);
+                    }
+                    sb.Append(" }");
+                }
+                return sb.ToString();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ solution/I/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PPWCode.Util.OddsAndEnds.I")]
+[assembly: AssemblyDescription("Main code of the PPWCode OddsAndEnds Util library")]
+[assembly: AssemblyConfiguration("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f6132d24-8001-4bcc-96b0-6901a610e5bc")]
