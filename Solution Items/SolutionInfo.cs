fe372547ad5d32f8e81da58dc4b8732eb15ad291
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2011-05-05 06:52:37 +0000

    v1.7
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Util/OddsAndEnds/trunk@4973 463e38ab-4f4e-0410-907a-65d2b3888964

--- Solution Items/SolutionInfo.cs
+++ Solution Items/SolutionInfo.cs
@@ -15,4 +15,4 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.6.*")]
+[assembly: AssemblyVersion("1.7.*")]
