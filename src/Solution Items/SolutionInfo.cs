0171eeef4edf021f3beea12e591906b9720cdcac
Kristel Bogaerts <kristel_bogaerts@peopleware.be>
2012-02-27 15:48:23 +0000

    Fix trunk on version 0.0.0.
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Util/OddsAndEnds/trunk@6103 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/Solution Items/SolutionInfo.cs
+++ src/Solution Items/SolutionInfo.cs
@@ -7,4 +7,4 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
