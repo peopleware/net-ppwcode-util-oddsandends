ed9ad51f8d2b008f22a2f1c6ec4abba02030ab77
Kristel Bogaerts <kristel_bogaerts@peopleware.be>
2012-08-14 14:42:42 +0000

    SEMPERA-2436 - new DateTimeProvider classes
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Util/OddsAndEnds/trunk@6308 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ src/I/DateTimeProvider/DefaultDateTimeProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PPWCode.Util.OddsAndEnds.I.DateTimeProvider
+{
+    public class DefaultDateTimeProvider : DateTimeProvider
+    {
+        #region Overrides of DateTimeProvider
+
+        public override DateTime Today
+        {
+            get { return DateTime.Today; }
+        }
+
+        public override DateTime Now
+        {
+            get { return DateTime.Now; }
+        }
+
+        #endregion
+    }
+}
