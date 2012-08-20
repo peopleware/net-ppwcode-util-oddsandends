2429f3e9676f360302607658cbe980b803518de8
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2012-08-20 15:35:50 +0000

    SEMPERA-2436 - implementation of a DateTimeProvider
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Util/OddsAndEnds/trunk@6313 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ src/I/DateTimeProvider/DefaultDateTimeProvider.cs
@@ -0,0 +1,25 @@
+﻿#region Using
+
+using System;
+
+#endregion
+
+namespace PPWCode.Util.OddsAndEnds.I.DateTimeProvider
+{
+    public class DefaultDateTimeProvider : DateTimeProvider
+    {
+        #region Overrides of DateTimeProvider
+
+        public override DateTime Today
+        {
+            get { return DateTime.Today; }
+        }
+
+        public override DateTime Now
+        {
+            get { return DateTime.Now; }
+        }
+
+        #endregion
+    }
+}
---
+++ src/I/DateTimeProvider/LambdaDateTimeProvider.cs
@@ -0,0 +1,36 @@
+﻿#region Using
+
+using System;
+
+using PPWCode.Util.OddsAndEnds.I.Extensions;
+
+#endregion
+
+namespace PPWCode.Util.OddsAndEnds.I.DateTimeProvider
+{
+    public class LambdaDateTimeProvider : DateTimeProvider
+    {
+        /// <summary>
+        /// LambdaDateTimeProvider x = new LambdaDateTimeProvider();
+        /// x.LambdaToday = () => return DateTime.Today
+        /// x.LambdaNow = () => return DateTime.Now
+        /// DateTimeProvider.Current = x;
+        /// </summary>
+
+        public Func<DateTime> LambdaNow { get; set; }
+
+        #region Overrides of DateTimeProvider
+
+        public override DateTime Today
+        {
+            get { return LambdaNow().StripHours(); }
+        }
+
+        public override DateTime Now
+        {
+            get { return LambdaNow(); }
+        }
+
+        #endregion
+    }
+}
