c0088c03a81bd12d592a7d6aff1ebe38862d7b08
Danny Van den Wouwer <danny_van_den_wouwer@peopleware.be>
2014-05-13 18:12:00 +0000

    nuget support
    git-svn-id: http://ppwcode.googlecode.com/svn/dotnet/Util/OddsAndEnds/dev/PPWCode.Util.OddsAndEnds.II@7547 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/CreateNuGetPackages.cmd
+++ src/CreateNuGetPackages.cmd
@@ -3,12 +3,23 @@
 if [%1]==[] goto usage
 
 setlocal
+    set project=PPWCode.Util.OddsAndEnds.II
 	set package_project=
 	if [%1]==[Debug] set package_project=1
 	if [%1]==[Release] set package_project=1
 	if defined package_project (
-		echo Packiging PPWCode.Util.OddsAndEnds.II ...
-		nuget pack .\II\PPWCode.Util.OddsAndEnds.II.csproj -Build -Properties "Configuration=%1"
+		echo Packiging %project% ...
+		nuget pack .\II\%project%.csproj -Build -Properties "Configuration=%1"
+	)
+	
+	set publish_package=
+	if not [%2]==[] set publish_package=1
+	if defined publish_package (
+		if not exist %project%.%3.nupkg echo nuget package %project%.%3.nupkg not found
+		if exist %project%.%3.nupkg (
+			echo Publish %project%.%3.nupkg to source %2
+			nuget push %project%.%3.nupkg -source %2
+		)
 	)
 endlocal
 goto eob
@@ -16,8 +27,10 @@ goto eob
 :usage
 echo.
 echo REQ p1=Specifiy configuration build (Debug, Release, ...)
+echo OPT p2=Publish to nuget source (local, ...)
+echo OPT p3=Version of package (1.0.0
 echo.
 echo Example:
-echo   call CreateNuGetPackage Release
+echo   call CreateNuGetPackage Release local 1.0.0
 echo.
 :eob
