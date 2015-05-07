22bcb653dd116d31fb631ebac15f88c63a993295
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2015-05-07 21:23:00 +0200

    Updated versions of the psake script files.

--- default.ps1
+++ default.ps1
@@ -1,4 +1,19 @@
-﻿# Copyright 2014 by PeopleWare n.v..
+﻿###############################################################################
+###  PSAKE build script                                                     ###
+###############################################################################
+###  Copyright 2015 by PeopleWare n.v.                                      ###
+###############################################################################
+###  Authors: Ruben Vandeginste                                             ###
+###############################################################################
+###                                                                         ###
+###  A psake build file that configures                                     ###
+###     - build and clean the solution                                      ###
+###     - restore nuget packages                                            ###
+###     - create and publish nuget packages                                 ###
+###     - run unit tests                                                    ###
+###     - generate documentation                                            ###
+###                                                                         ###
+###############################################################################
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,47 +27,122 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#
-# psake build file
-#
 
-Framework "4.0"
+#region CONFIGURATION
 
+###############################################################################
+### CONFIGURATION                                                           ### 
+###############################################################################
+
+###############################################################################
+# Use build tools from .NET framework 4.0
+#
+Framework '4.0'
+
+###############################################################################
+# Properties that are used by this psake
+# script; these can be overridden when
+# invoking this script
+#
 Properties {
-    $configuration = "Debug"
+    # buildconfig: the configuration used for building 
+    $buildconfig = 'Debug'
+    # repos: ordered array of nuget repositories to use for fetching packages
     $repos = @("local", "nuget")
-    $publishrepo = "local"
+    # publishrepo: nuget repository to use for publishing a new package
+    $publishrepo = 'local'
+    # chatter: number to indicate verbosity during the execution of tasks
+    #   0 - no output
+    #   1 - minimal output, tasks executed
+    #   2 - extra output, steps within tasks
+    #   3 - even more output
+    $chatter = 1
+    # chattercolor: foreground color to use for task output
+    $chattercolor = 'Green'
+    # nugetcache: by default, disable the nuget cache
+    $usenugetcache = $false
 }
 
-# Properties description
+#endregion
+
+#region PRIVATE HELPERS
+
+###############################################################################
+### PRIVATE HELPERS                                                         ###
+###############################################################################
+
+###############################################################################
+# Helper for printing out documentation
+#
 function PropertyDocumentation() {
-    Write-Host "Properties"
-    Write-Host "----------"
-    Write-Host "`$configuration   The build configuration."
-    Write-Host "`$repos           Array of repositories to use as source repositories."
-    Write-Host "`$publishrepo     The repository to use for publishing generated packages."
+    Write-Host 'Properties'
+    Write-Host '----------'
+    Write-Host "`$buildconfig     Build configuration."
+    Write-Host "`$repos           Ordered array of repositories to use as nuget source repositories."
+    Write-Host "`$publishrepo     The repository to use for publishing generated nuget packages."
+    Write-Host "`$chatter         The level of verbosity during task execution."
+    Write-Host  '             0 - No output'
+    Write-Host  '             1 - Minimal output, tasks executed'
+    Write-Host  '             2 - Extra output, steps within tasks'
+    Write-Host  '             3 - Even more output, debug execution'
+    Write-Host "`$chattercolor    Foreground color to use for task output."
+    Write-Host "`$usenugetcache   Boolean to indicate whether the NuGet built-in cache should be used (default=$false)."
 }
 
-# Default is help
+###############################################################################
+# Helper for chatter
+#
+function Chatter {
+    param
+    (
+        [string]
+        $msg = '.',
+
+        [int]
+        $level = 3
+    )
+
+    if ($level -le $chatter) {
+        Write-Host $msg -ForegroundColor $chattercolor
+    }
+}
+
+#endregion
+
+
+#region TASKS
+
+###############################################################################
+### TASKS                                                                   ###
+###############################################################################
+
+###############################################################################
+# Default task, executed when no task is
+# given explicitly
+#
 Task Default -depends ?
 
-# Show help
-Task ? -description "Show help." {
+###############################################################################
+# Help
+#
+Task ? -description 'Show help.' {
     Write-Host
     PropertyDocumentation 
     WriteDocumentation
 }
 
+###############################################################################
 # Clean ReSharper cache folder
-Task ReSharperClean -description "Clean ReSharper cache folder in the solution." {
-    Push-Location
+#
+Task ReSharperClean -description 'Clean ReSharper cache folder in the solution.' {
 
+    Chatter 'Clean ReSharper cache.' 1
+
+    Push-Location
     try
     {
-        #Write-Host "Clean ReSharper cache." -ForegroundColor Green
-
         # clean up cache folder
-        Remove-Item -Path 'src\_ReSharper*' -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path 'src\_ReSharper*' -Recurse -Force
     }
     finally
     {
@@ -60,16 +150,18 @@ Task ReSharperClean -description "Clean ReSharper cache folder in the solution."
     }
 }
 
+###############################################################################
 # Clean packages folder
-Task PackageClean -description "Clean packages folder in the solution." -depends ReSharperClean {
-    Push-Location
+#
+Task PackageClean -description 'Clean packages folder in the solution.' -depends ReSharperClean {
 
+    Chatter 'Clean package dependencies.' 1
+
+    Push-Location
     try
     {
-        #Write-Host "Clean package dependencies." -ForegroundColor Green
-
         # clean up packages folder
-        Remove-Item -Path 'src\packages\*' -Exclude 'repositories.config' -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path 'src\packages\*' -Exclude 'repositories.config' -Recurse -Force
     }
     finally
     {
@@ -77,21 +169,28 @@ Task PackageClean -description "Clean packages folder in the solution." -depends
     }
 }
 
+###############################################################################
 # Restore packages
-Task PackageRestore -description "Restore nuget package dependencies." -depends PackageClean {
-    Push-Location
+#
+Task PackageRestore -description 'Restore nuget package dependencies.' -depends PackageClean {
 
+    Chatter 'Restoring package dependencies.' 1
+
+    Push-Location
     try
     {
-        #Write-Host "Restoring package dependencies." -ForegroundColor Green
-
         Set-Location 'src'
         $solution = Get-Item '*.sln' | Select-Object -First 1
-        $reposources = ""
+        $reposources = ''
         $repos | ForEach-Object { $reposources="$reposources -source $_" } 
-        #Exec { & nuget "restore $solution $reposources" }
-        $args="restore $solution $reposources"
-        Start-Process -Wait -FilePath nuget -ArgumentList $args -NoNewWindow
+
+        $nocache = ''
+        if (-not $usenugetcache) {
+            $nocache = '-NoCache'
+        }
+
+        Chatter "nuget restore $solution $reposources" 3
+        Exec { Invoke-Expression "nuget restore $solution $reposources $nocache -NonInteractive -Verbosity quiet" }
     }
     finally
     {
@@ -99,17 +198,20 @@ Task PackageRestore -description "Restore nuget package dependencies." -depends 
     }
 }
 
+###############################################################################
 # Build the solution
-Task Build -description "Build the solution." -depends Clean {
-    Push-Location
+#
+Task Build -description 'Build the solution.' -depends Clean {
 
+    Chatter 'Building the solution.' 1
+    
+    Push-Location
     try
     {
-        #Write-Host "Compiling." -ForegroundColor Green
-
         Set-Location 'src'
         $solution = Get-Item '*.sln' | Select-Object -First 1
-        Exec { msbuild "$($solution.Name)" /t:Build /m /p:Configuration=$configuration /v:quiet /nologo }
+        Chatter "MSBuild: $($solution.Name) /t:Build /m /nr:false /p:Configuration=$buildconfig /v:quiet /nologo." 3
+        Exec { msbuild "$($solution.Name)" /t:Build /m /nr:false /p:Configuration=$buildconfig /v:quiet /nologo }
     }
     finally
     {
@@ -117,29 +219,42 @@ Task Build -description "Build the solution." -depends Clean {
     }
 }
 
+###############################################################################
 # Clean build artifacts and temporary files
-Task Clean -description "Clean build output and generated packages." {
-    Push-Location
+#
+Task Clean -description 'Clean build output and generated packages.' {
 
+    Chatter 'Cleaning.' 1
+
+    Push-Location
     try
     {
-        #Write-Host "Cleaning." -ForegroundColor Green
-
         # msbuild clean
+        Chatter 'Cleaning solution' 2
         Set-Location 'src'
         $solution = Get-Item '*.sln' | Select-Object -First 1
-        Exec { msbuild "$($solution.Name)" /t:Clean /m /p:Configuration=$configuration /v:quiet /nologo }
+        Chatter "MSBuild: $($solution.Name) /t:Clean /m /nr:false /p:Configuration=$buildconfig /v:quiet /nologo." 3
+        Exec { msbuild "$($solution.Name)" /t:Clean /m /nr:false /p:Configuration=$buildconfig /v:quiet /nologo }
         Set-Location '..'
 
         # clean up scratch folder
-        Remove-Item -Path 'scratch' -Recurse -Force -ErrorAction Ignore
+        Chatter 'Cleaning scratch' 2
+        Remove-Item -Path 'scratch' -Recurse -Force
 
         # clean up bin/obj folders
+        Chatter 'Cleaning bin/obj' 2
         Set-Location 'src'
-        Remove-Item -Path '*\bin','*\obj' -Recurse -Force -ErrorAction Ignore
-
-        # clean up packages folder
-        Get-ChildItem -Directory | Where-Object { $_.Name -ne 'packages'  } |  Get-ChildItem -Filter '*.nupkg' -recurse | Remove-Item -Force -ErrorAction Ignore
+        Remove-Item -Path '*\bin','*\obj' -Recurse -Force
+        Set-Location '..'
+        
+        # clean up generated nuget packages
+        Chatter 'Cleaning generated packages' 2
+        Set-Location 'src'
+        Get-ChildItem -Directory |
+            Where-Object { $_.Name -ne 'packages'  } |
+            Get-ChildItem -Filter '*.nupkg' -recurse |
+            Foreach-Object { Remove-Item -LiteralPath $_.FullName }
+        Set-Location '..'
     }
     finally
     {
@@ -147,30 +262,37 @@ Task Clean -description "Clean build output and generated packages." {
     }
 }
 
+###############################################################################
 # Full clean
-Task RealClean -description "Clean build output, generated packages and packages folder." -depends Clean,PackageClean
+#
+Task FullClean -description 'Clean build output, generated packages and packages folder.' -depends Clean,PackageClean
 
+###############################################################################
 # Full build
-Task FullBuild -description "Do a full build starting from a clean solution." -depends PackageRestore,Build
+#
+Task FullBuild -description 'Do a full build starting from a clean solution.' -depends PackageRestore,Build
 
-# Create Nuget package
-Task Package -description "Generate the packages from a clean build, and publish the packages." -depends FullBuild {
+###############################################################################
+# Create Nuget packages
+#
+Task Package -description 'Generate the packages from a clean build, and publish the packages.' -depends FullBuild {
+
+    Chatter 'Packaging.' 1
+
     Push-Location
-
     try
     {
-        #Write-Host "Packaging." -ForegroundColor Green
-
         Set-Location 'src'
         $solution = Get-Item '*.sln' | Select-Object -First 1
 
         # build
         $nuspecfiles = Get-ChildItem -Directory | Where-Object { $_.Name -ne 'packages'  } |  Get-ChildItem -Filter '*.nuspec' -recurse
         $nuspecfiles | ForEach-Object {
+            Chatter "Packaging: $_.BaseName" 2
             Set-Location $_.DirectoryName
-            Exec { nuget pack "$($_.BaseName).csproj" -Build -Properties "Configuration=$configuration" -Verbosity quiet }
-            $nupkgfiles = Get-ChildItem -File -Filter "*.nupkg"
-            Exec { nuget push "$($nupkgfiles[0].Name)" -source $publishrepo }
+            Exec { NuGet.exe pack "$($_.BaseName).csproj" -Build -Properties "Configuration=$buildconfig" -Verbosity quiet }
+            $nupkgfiles = Get-ChildItem -File -Filter '*.nupkg'
+            Exec { NuGet.exe push "$($nupkgfiles[0].Name)" -source $publishrepo }
         }
     }
     finally
@@ -178,3 +300,81 @@ Task Package -description "Generate the packages from a clean build, and publish
         Pop-Location
     }
 }
+
+###############################################################################
+# Run unit tests
+#
+Task Test -description 'Run the unit tests using NUnit console test runner.' -depends FullBuild {
+
+    Chatter 'Testing.' 1
+
+    Push-Location
+    try
+    {
+        # find nunit-console runner
+        Chatter 'Searching nunit-console runner.' 2
+        $runner = Get-ChildItem -Path 'src\packages' -Filter 'nunit-console.exe' -Recurse | Select-Object -First 1
+        
+        # find unit test dlls
+        Chatter 'Searching test projects.' 2
+        $testprojects = Get-ChildItem -Directory -Path 'src' | Where-Object { $_.Name -ne 'packages' } | Get-ChildItem -Filter '*Test*.csproj' -Recurse
+        $testdlls = $testprojects | ForEach-Object { Get-ChildItem -Path "scratch\bin\$($_.BaseName)-*" -Filter "$($_.BaseName).dll" -Recurse | Select-Object -First 1 }
+        
+        # execute runner, one run for each unit test
+        foreach ($testdll in $testdlls) {
+            # generate folder for test output
+            $dummy = New-Item -ItemType Directory -Path (Join-Path 'scratch\nunit\' "$($testdll.BaseName)")
+        
+            # execute runner
+            $testrunnerargs = @(
+                "/work:scratch\nunit\$($testdll.BaseName)"
+                '/framework:net-4.0'
+                '/result:nunit-test-results.xml'
+                '/out:nunit-stdout.txt'
+                '/err:nunit-stderr.txt'
+                "$($testdll.FullName)"
+                )
+
+            Chatter "Test runner exe: $($runner.FullName)" 3
+            Chatter 'Test runner args:' 3
+            foreach ($arg in $testrunnerargs) {
+                Chatter "     $arg" 3
+            }
+            
+            Exec { & "$($runner.FullName)" $testrunnerargs }
+        }
+    }
+    finally
+    {
+        Pop-Location
+    }
+}
+
+###############################################################################
+# Generate documentation
+#
+Task Documentation -description 'Generate documentation using Sandcastle Help File Builder.' -depends FullBuild {
+
+    Chatter 'Documenting.' 1
+
+    Push-Location
+    try
+    {
+        Set-Location 'src'
+        $solution = Get-Item '*.sln' | Select-Object -First 1
+
+        # build
+        $shfbfiles = Get-ChildItem -Directory | Where-Object { $_.Name -ne 'packages'  } |  Get-ChildItem -Filter '*.shfbproj' -recurse
+        $shfbfiles | ForEach-Object {
+            Chatter "Documenting: $_.BaseName" 2
+            Set-Location $_.DirectoryName
+            Exec { msbuild "$($_.Name)" /t:Build /nr:false /p:Configuration=$buildconfig /v:quiet /nologo }
+        }
+    }
+    finally
+    {
+        Pop-Location
+    }
+}
+
+#endregion
--- init-psake.ps1
+++ init-psake.ps1
@@ -1,4 +1,14 @@
-﻿# Copyright 2014 by PeopleWare n.v..
+﻿###############################################################################
+###  Bootstrap script for PSAKE                                             ###
+###############################################################################
+###  Copyright 2015 by PeopleWare n.v..                                     ###
+###############################################################################
+###  Authors: Ruben Vandeginste                                             ###
+###############################################################################
+###                                                                         ###
+###  A script to bootstrap the powershell session for psake.                ###
+###                                                                         ###
+###############################################################################
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,43 +22,137 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-param([string]$target = "")
+#region INPUT PARAMATERS
 
+###############################################################################
+### INPUT PARAMETERS                                                        ### 
+###############################################################################
+
+###############################################################################
+# input parameters
+#   target: Task to execute
+#   repos:  NuGet repositories to be used when psake is not available yet
+param([string]$target = '', [string[]]$repos = @())
+
+#endregion
+
+
+#region HELPERS
+
+###############################################################################
+### HELPERS                                                                 ### 
+###############################################################################
+
+###############################################################################
+# Stolen from Psake, helper function to execute external commands and respect
+# their exit code.
+#
+function Exec {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, Mandatory = 1)][scriptblock]$cmd,
+        [Parameter(Position = 1, Mandatory = 0)][string]$errorMessage = 'Bad command.',
+        [Parameter(Position = 2, Mandatory = 0)][int]$maxRetries = 0,
+        [Parameter(Position = 3, Mandatory = 0)][string]$retryTriggerErrorPattern = $null
+    )
+
+    $tryCount = 1
+
+    do
+    {
+        try
+        {
+            $global:lastexitcode = 0
+            & $cmd
+            if ($lastexitcode -ne 0)
+            {
+                throw ('Exec: ' + $errorMessage)
+            }
+            break
+        }
+        catch [Exception]
+        {
+            if ($tryCount -gt $maxRetries)
+            {
+                throw $_
+            }
+            
+            if ($retryTriggerErrorPattern -ne $null)
+            {
+                $isMatch = [regex]::IsMatch($_.Exception.Message, $retryTriggerErrorPattern)
+                
+                if ($isMatch -eq $false)
+                {
+                    throw $_
+                }
+            }
+            
+            Write-Host "Try $tryCount failed, retrying again in 1 second..."
+            
+            $tryCount++
+            
+            [System.Threading.Thread]::Sleep([System.TimeSpan]::FromSeconds(1))
+        }
+    }
+    while ($true)
+}
+
+#endregion
+
+
+#region MAIN
+
+###############################################################################
+### MAIN                                                                    ### 
+###############################################################################
+
+###############################################################################
+# Main script to bootstrap psake.
+#
 try 
 {
     # execution policy for scripts
     Set-ExecutionPolicy RemoteSigned
 
-    # find module
+    # find module, if not found, try to download it
     $modules = Get-Item  .\src\packages\psake.*\tools\psake.psm1
     if ($modules -eq $null)
     {
         Push-Location
         Set-Location 'src'
-        & nuget restore
+        
+        $reposources = ''
+        $repos | ForEach-Object { $reposources="$reposources -source $_" }
+        Exec { Invoke-Expression "nuget restore $reposources -noninteractive -nocache -verbosity quiet" } 'NuGet not available, or not executing as expected.'
+        
         Pop-Location
 
         $modules = Get-Item  .\src\packages\psake.*\tools\psake.psm1
         if ($modules -eq $null)
         {
-            throw "Cannot find psake module."
+            throw 'Cannot find or fetch psake module.'
         }
     }
 
     # take most recent module, if multiple found
+    #   not completely correct, but 'good enough'
     $module = $modules | Sort-Object -Property FullName -Descending | Select-Object -First 1
 
     # import module, force a reload if already loaded
     Import-Module $module.FullName -Force
-    if ($target -ne "")
+    
+    # execute the target, if any given
+    if ($target -ne '')
     {
         Invoke-psake $target
     }
 }
 catch 
 {
-    Write-Host "Error executing psake.ps1"
-    Write-Host "Error:"
-    Write-Host $_
+    Write-Host 'Error executing psake.ps1' -ForegroundColor DarkYellow
     Write-Host
+    # Re-Throw so that the calling code does not continue.
+    throw $_
 }
+
+#endregion
