efc643e39013402a0674e2b859c06e31ac4cae7f
Guy Stals <Guy_Stals@peopleware.be>
2015-05-13 10:28:36 +0200

    add tests to ValidationTests

--- src/Test.II/ValidationFactory.cs
+++ src/Test.II/ValidationFactory.cs
@@ -23,7 +23,24 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
 
         public static IEnumerable ValidRrn
         {
-            get { yield return new TestCaseData(string.Empty).Returns(false); }
+            get
+            {
+                yield return new TestCaseData(string.Empty).Returns(false);
+                yield return new TestCaseData("111111111111").Returns(false);
+                yield return new TestCaseData("72020290080").Returns(false);
+
+                // controle geboortedatum voor 01/01/2000
+                yield return new TestCaseData("67 06 29 389 93").Returns(true);
+                yield return new TestCaseData("67.06.29.389.93").Returns(true);
+                yield return new TestCaseData("67062938993").Returns(true); 
+
+                // controle geboortedatum na 01/01/2000
+                yield return new TestCaseData("00 09 15 011 19  ").Returns(true);
+                yield return new TestCaseData("00091501119").Returns(true);
+
+                // todo Ruben: willen we dat "00Q09IsZot15 011 19  " een geldig nummer is ?
+                yield return new TestCaseData("00Q09IsZot15 011 19  ").Returns(false);
+            }
         }
 
         public static IEnumerable StrictValidRrn
@@ -31,6 +48,18 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
             get
             {
                 yield return new TestCaseData(string.Empty).Returns(false);
+                yield return new TestCaseData("111111111111").Returns(false);
+                yield return new TestCaseData("72020290080").Returns(false);
+                yield return new TestCaseData("00Q09IsZot15 011 19  ").Returns(false);
+
+                // controle geboortedatum voor 01/01/2000
+                yield return new TestCaseData("67 06 29 389 93").Returns(false);
+                yield return new TestCaseData("67.06.29.389.93").Returns(false);
+                yield return new TestCaseData("67062938993").Returns(true);
+
+                // controle geboortedatum na 01/01/2000
+                yield return new TestCaseData("00 09 15 011 19  ").Returns(false);
+                yield return new TestCaseData("00091501119").Returns(true);              
             }
         }
 
@@ -39,6 +68,7 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
             get
             {
                 yield return new TestCaseData(string.Empty).Returns(string.Empty);
+                yield return new TestCaseData("11").Returns("00000000011");
             }
         }
 
@@ -47,6 +77,14 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
             get
             {
                 yield return new TestCaseData(string.Empty).Returns(null);
+
+                // controle geboortedatum voor 01/01/2000
+                yield return new TestCaseData("67.06.29.389.93").Returns(new DateTime(1967, 06, 29));
+                yield return new TestCaseData("67062938993").Returns(new DateTime(1967, 06, 29));
+
+                // controle geboortedatum na 01/01/2000
+                yield return new TestCaseData("00 09 15 011 19  ").Returns(new DateTime(2000, 09, 15));
+                yield return new TestCaseData("00091501119").Returns(new DateTime(2000, 09, 15));
             }
         }
 
@@ -55,6 +93,14 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
             get
             {
                 yield return new TestCaseData(string.Empty).Returns(string.Empty);
+                yield return new TestCaseData("123").Returns("123");
+                yield return new TestCaseData("a123").Returns("123");
+                yield return new TestCaseData("1a23").Returns("123");
+                yield return new TestCaseData("123a").Returns("123");
+                yield return new TestCaseData(" 123").Returns("123");
+                yield return new TestCaseData("a1jjjj23").Returns("123");
+                yield return new TestCaseData("123ddd").Returns("123");
+                yield return new TestCaseData("ddd1////23?,.^$#").Returns("123");
             }
         }
 
@@ -63,6 +109,14 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
             get
             {
                 yield return new TestCaseData(string.Empty).Returns(false);
+                yield return new TestCaseData("010010108").Returns(true);
+                yield return new TestCaseData("0.100.101 08").Returns(true);
+                yield return new TestCaseData("010010109").Returns(false);
+                yield return new TestCaseData("10010108").Returns(true);
+                yield return new TestCaseData("951068994").Returns(true);
+                yield return new TestCaseData("951068984").Returns(false);
+                yield return new TestCaseData("5109839040").Returns(true);
+                yield return new TestCaseData("5109839041").Returns(false);
             }
         }
 
@@ -71,6 +125,14 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
             get
             {
                 yield return new TestCaseData(string.Empty).Returns(false);
+                yield return new TestCaseData("010010108").Returns(false);
+                yield return new TestCaseData("0.100.101 08").Returns(false);
+                yield return new TestCaseData("010010109").Returns(false);
+                yield return new TestCaseData("10010108").Returns(false);
+                yield return new TestCaseData("951068994").Returns(false);
+                yield return new TestCaseData("951068984").Returns(false);
+                yield return new TestCaseData("5109839040").Returns(true);
+                yield return new TestCaseData("5109839041").Returns(false);
             }
         }
 
@@ -79,6 +141,7 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
             get
             {
                 yield return new TestCaseData(string.Empty).Returns(string.Empty);
+                yield return new TestCaseData("483").Returns("0000000483");
             }
         }
 
@@ -87,6 +150,15 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
             get
             {
                 yield return new TestCaseData(string.Empty).Returns(false);
+                yield return new TestCaseData("896073528").Returns(true);
+                yield return new TestCaseData("8.9607.3528").Returns(true);
+                yield return new TestCaseData("8 9607 3528").Returns(true);
+                yield return new TestCaseData("2154199061").Returns(true);
+                yield return new TestCaseData("0899754677").Returns(true);
+                yield return new TestCaseData("0000000000").Returns(false);
+                yield return new TestCaseData("0204908936").Returns(true);
+                yield return new TestCaseData("0204908937").Returns(false);
+                yield return new TestCaseData("0204908947").Returns(false);
             }
         }
 
@@ -95,6 +167,15 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
             get
             {
                 yield return new TestCaseData(string.Empty).Returns(false);
+                yield return new TestCaseData("896073528").Returns(false);
+                yield return new TestCaseData("8.9607.3528").Returns(false);
+                yield return new TestCaseData("8 9607 3528").Returns(false);
+                yield return new TestCaseData("2154199061").Returns(true);
+                yield return new TestCaseData("0899754677").Returns(true);
+                yield return new TestCaseData("0000000000").Returns(false);
+                yield return new TestCaseData("0204908936").Returns(true);
+                yield return new TestCaseData("0204908937").Returns(false);
+                yield return new TestCaseData("0204908947").Returns(false);
             }
         }
 
@@ -103,6 +184,7 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
             get
             {
                 yield return new TestCaseData(string.Empty).Returns(string.Empty);
+                yield return new TestCaseData("1").Returns("0000000001");
             }
         }
 
@@ -111,6 +193,13 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
             get
             {
                 yield return new TestCaseData(string.Empty).Returns(false);
+                yield return new TestCaseData("BE 896415503").Returns(true);
+                yield return new TestCaseData("899997573").Returns(true);
+                yield return new TestCaseData("899842571").Returns(true);
+                yield return new TestCaseData("899475951").Returns(true);
+                yield return new TestCaseData("000000000").Returns(false);
+                yield return new TestCaseData("400 833 296").Returns(true);
+                yield return new TestCaseData("400000680").Returns(true);
             }
         }
 
@@ -119,6 +208,9 @@ namespace PPWCode.Util.OddsAndEnds.Test.II
             get
             {
                 yield return new TestCaseData(string.Empty).Returns(false);
+                yield return new TestCaseData("400 833 296").Returns(false);
+                yield return new TestCaseData("400000680").Returns(true);
+                yield return new TestCaseData("899997573").Returns(true);
             }
         }
     }
