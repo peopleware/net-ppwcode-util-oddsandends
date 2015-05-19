dc6eb0953ae66bbff581908940d4433dac13309b
Guy Stals <Guy_Stals@peopleware.be>
2015-05-19 13:53:32 +0200

    add tests for assemblyhelper

--- src/Test.II/AssemblyHelperTests.cs
+++ src/Test.II/AssemblyHelperTests.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 using NUnit.Framework;
 
+using PPWCode.Util.OddsAndEnds.II.AssemblyHelpers;
+
 namespace PPWCode.Util.OddsAndEnds.Test.II
 {
     [TestFixture]
     public class AssemblyHelperTests
     {
+        [Test]
+        public void LoadAssemblyNotNullTest()
+        {
+            var assembly = AssemblyHelper.LoadAssembly("PPWCode.Util.OddsAndEnds.II.dll");
+            Assert.IsNotNull(assembly);
+        }
+
+        [Test]
+        public void CreateInstanceOfSequenceGeneratorIsNotNull()
+        {
+            var assembly = AssemblyHelper.LoadAssembly("PPWCode.Util.OddsAndEnds.II.dll");
+            var adSearch = AssemblyHelper.CreateInstanceOf(assembly, "PPWCode.Util.OddsAndEnds.II.UnitTestHelpers.SequenceGenerator");// "AdSearch");
+            Assert.IsNotNull(adSearch);
+        }
     }
 }
