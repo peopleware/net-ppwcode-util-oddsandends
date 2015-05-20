4ec026b934a59f9c42fa5029204b670af9366b01
Guy Stals <Guy_Stals@peopleware.be>
2015-05-20 11:02:35 +0200

    add comment to GenericSerializationInfo

--- src/II/Serialization/GenericSerializationInfo.cs
+++ src/II/Serialization/GenericSerializationInfo.cs
@@ -23,16 +23,32 @@ namespace PPWCode.Util.OddsAndEnds.II.Serialization
     {
         private readonly SerializationInfo m_SerializationInfo;
 
+        /// <summary>
+        /// The constructor.
+        /// </summary>
+        /// <param name="info">The SerializationInfo.</param>
         public GenericSerializationInfo(SerializationInfo info)
         {
             m_SerializationInfo = info;
         }
 
+        /// <summary>
+        /// Adds a value into the SerializationInfo store.
+        /// </summary>
+        /// <typeparam name="T">The type declaration.</typeparam>
+        /// <param name="name">The name to associate with the value.</param>
+        /// <param name="value">The value to be serialized.</param>
         public void AddValue<T>(string name, T value)
         {
             m_SerializationInfo.AddValue(name, value, typeof(T));
         }
 
+        /// <summary>
+        /// Retrieves a value from the SerializationInfo store.
+        /// </summary>
+        /// <typeparam name="T">The type declaration.</typeparam>
+        /// <param name="name">The name associated with the value to retrieve.</param>
+        /// <returns>The object of the specified Type associated with the name.</returns>
         public T GetValue<T>(string name)
         {
             object obj = m_SerializationInfo.GetValue(name, typeof(T));
