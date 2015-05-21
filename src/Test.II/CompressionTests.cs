6b89a1dfbbe4dbba50159d0bde49290f385d4481
Guy Stals <Guy_Stals@peopleware.be>
2015-05-21 10:09:41 +0200

    add some compressionTests

--- src/Test.II/CompressionTests.cs
+++ src/Test.II/CompressionTests.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 using NUnit.Framework;
 
+using PPWCode.Util.OddsAndEnds.II.Streaming;
+
 namespace PPWCode.Util.OddsAndEnds.Test.II
 {
     [TestFixture]
     public class CompressionTests
     {
+        [Test]
+        public void CompressedByteArrayNotNull()
+        {
+            Random random = new Random();
+            Byte[] array = new byte[10];
+            random.NextBytes(array);
+
+            var result = Compression.Compress(array);
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void CompressThenDecompressReturnsSameArray()
+        {
+            Random random = new Random();
+            Byte[] array = new byte[10];
+            random.NextBytes(array);
+
+            var compressed = Compression.Compress(array);
+            var result = Compression.DeCompress(compressed);
+
+            Assert.AreEqual(array, result);
+        }
     }
 }
