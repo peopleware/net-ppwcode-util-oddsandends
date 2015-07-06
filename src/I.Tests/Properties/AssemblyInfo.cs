0485b7628e21749c8875fd1bef55851725f71ea3
Ruben Vandeginste <ruben.vandeginste@gmail.com>
2015-07-06 22:45:32 +0200

    Cleanup AssemblyInfo and resources files.

--- src/I.Tests/Properties/AssemblyInfo.cs
+++ src/I.Tests/Properties/AssemblyInfo.cs
@@ -1,12 +1,8 @@
-﻿#region Using
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-#endregion
-
-[assembly: AssemblyTitle("PPWCode.Vernacular.Exceptions.Test_I")]
-[assembly: AssemblyDescription("Test code of the PPWCode Exceptions Vernacular")]
+[assembly: AssemblyTitle("PPWCode.Util.OddsAndEnds.I.Tests")]
+[assembly: AssemblyDescription("Unit tests for PPWCode.Util.OddsAndEnds.I")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8c52ee8f-d53d-42d9-b7ad-a1cf552e0006")]
