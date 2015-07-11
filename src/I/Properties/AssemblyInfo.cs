f093ffecc981676feea65612981758f457526f8e
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2015-07-11 22:43:37 +0200

    Fixup nuspec file.

--- src/I/Properties/AssemblyInfo.cs
+++ src/I/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("PPWCode.Util.OddsAndEnds.I")]
-[assembly: AssemblyDescription("PPWCode.Util.OddsAndEnds.I library")]
+[assembly: AssemblyDescription("PPWCode.Util.OddsAndEnds.I")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f6132d24-8001-4bcc-96b0-6901a610e5bc")]
