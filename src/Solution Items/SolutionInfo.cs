fb11c21277a79922b7149aa98227ba23d9669cbe
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2015-07-11 22:43:58 +0200

    Bumping version for new release.

--- src/Solution Items/SolutionInfo.cs
+++ src/Solution Items/SolutionInfo.cs
@@ -16,9 +16,9 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Peopleware NV")]
 [assembly: AssemblyProduct("PPWCode Util OddsAndEnds")]
-[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
+[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.3.1.0")]
-[assembly: AssemblyInformationalVersion("2.3.1.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyInformationalVersion("2.4.0.0")]
