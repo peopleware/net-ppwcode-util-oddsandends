c7ec094a1e532464900222960e30bf44bc5cdbfb
Hans Van Gestel <hans_van_gestel@peopleware.be>
2016-05-11 16:22:00 +0200

    SEMPERA-3298: Bump version

--- src/Solution Items/SolutionInfo.cs
+++ src/Solution Items/SolutionInfo.cs
@@ -20,5 +20,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyInformationalVersion("1.4.0")]
